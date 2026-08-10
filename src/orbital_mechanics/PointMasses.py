10528916a7ba52f0b9b56302aa4637d79f5c25dc
Cameron B Comeau <cameron.b.comeau@hotmail.com>
2026-08-10 14:16:16 -0600

    Equation 1.6

--- src/orbital_mechanics/PointMasses.py
+++ src/orbital_mechanics/PointMasses.py
@@ -7,6 +7,7 @@ import math
 def vector_magnitude(ax: float = 0.0, ay: float = 0.0, az: float = 0.0) -> float:
     """
     Compute the magnitude of a 3D vector.
+    Equation 1.4, page 4
 
     Parameters
     ----------
@@ -23,3 +24,33 @@ def vector_magnitude(ax: float = 0.0, ay: float = 0.0, az: float = 0.0) -> float
         The Euclidean magnitude of the vector.
     """
     return math.sqrt(ax**2 + ay**2 + az**2)
+
+def vector_direction_angle(direction_magnitude: float = 0.0, vector_magnitude: float = 0.0, 
+                           return_degrees: bool = True ) -> float:
+    """
+    Computes the direction angle of a vector component.
+    Equation 1.6, page 4
+
+    Parameters
+    ----------
+    direction_magnitude : float
+        Component magnitude of vector.
+    vector_magnitude : float
+        Overall magnitude of vector.
+    return_degrees : bool
+        If set to true (default), then value returned is in degrees.
+
+    Returns
+    -------
+    float
+        Either the degrees or radians of the direction of the vector component
+
+    """
+    if vector_magnitude == 0.0:
+        # Divide by zero check
+        return 0.0
+    else:
+        if return_degrees == True:
+            return math.degrees( math.acos( direction_magnitude / vector_magnitude ) )
+        else:
+            return math.acos( direction_magnitude / vector_magnitude )
