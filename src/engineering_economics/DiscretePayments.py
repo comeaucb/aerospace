3b5f770c0892460c269c3d8bceed0f6133544bd0
Cameron B Comeau <cameron.b.comeau@hotmail.com>
2026-08-11 12:28:38 -0600

    Simple interest

--- src/engineering_economics/DiscretePayments.py
+++ src/engineering_economics/DiscretePayments.py
@@ -2,6 +2,30 @@
 
 import math
 
+def simple_interest_final( principal: float = 0.0,
+                           periods: float = 0.0,
+                           interest_rate: float = 0.0 ):
+    """
+    Calculate the final value after simple interest is applied.
+    Equation 3.2 page 65
+
+    Parameters
+    ----------
+    principal : float
+        Initial account value.
+    periods : float
+        Number of periods.
+    interest_rate : float
+        Interest rate in decimal form (e.g., 0.05 for 5%).
+
+    Returns
+    -------
+    float
+        Final value after compound interest.
+    """
+
+    return principal * ( 1 + interest_rate * periods )
+
 def compound_interest_final(
     principal: float = 0.0,
     periods: float = 0.0,
--- tests/test_DiscretePayments.py
+++ tests/test_DiscretePayments.py
@@ -2,6 +2,11 @@
 
 from engineering_economics.DiscretePayments import *
 
+def test_simple_interest_final():
+    assert simple_interest_final(0,0,0) == 0
+    assert simple_interest_final(10,10,0) == 10
+    assert simple_interest_final(10,10,0.1) == 20
+
 def test_compound_interest_final():
     assert compound_interest_final(10,10,0) == 10
     assert compound_interest_final(0,10,10) == 0
