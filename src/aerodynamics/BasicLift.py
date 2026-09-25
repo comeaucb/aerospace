d22bf97d6e9f7dbb2a1e93fb09533064e89359a6
Cameron B Comeau <cameron.b.comeau@hotmail.com>
2026-09-24 18:09:27 -0600

    Starting back at chapter 1.

---
+++ src/aerodynamics/BasicLift.py
@@ -0,0 +1,21 @@
+# filename: BasicLift.py
+# Implements equations from Chapter 01 Aerodynamics: Some Introductory Thoughts, 
+# from "Fundamentals of Aerodynamics: 5th Ed" by 
+# John D. Anderson, Jr, Copyright 2011 McGraw Hill
+
+import numpy as np
+from scipy.optimize import root_scalar
+
+def freestream_dynamic_pressure( fs_density: float=0.0, fs_velocity: float=0.0 ):
+    # Page 24
+    # Implements the free stream dynamics pressure; the pressure far ahead of 
+    # the airfoil (far enough that the airfoil being there has no effect)
+
+    # fs_density
+    # density of free stream fluid (kg/m³)
+    # fs_velocity
+    # velocity of fluid (m/s)
+    # return is free stream pressure (Pa)
+    return 0.5 * fs_density * pow(fs_velocity, 2)
+
+def coefficient_lift( lift_force: float=0.0, surface_area: float=0.0 ):
