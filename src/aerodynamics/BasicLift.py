8dcd383c9c08254077422b11a3fd17658927c507
Cameron B Comeau <cameron.b.comeau@hotmail.com>
2026-09-25 10:25:24 -0600

    Needs more tests after the refactoring.

--- src/aerodynamics/BasicLift.py
+++ src/aerodynamics/BasicLift.py
@@ -16,11 +16,23 @@ def freestream_dynamic_pressure( fs_density: float=0.0, fs_velocity: float=0.0 )
     # @return Free stream pressure (Pa)
     return 0.5 * fs_density * pow(fs_velocity, 2)
 
-def coefficient_lift( lift_force: float=0.0, surface_area: float=0.0, fs_dynamic_pressure: float=0.0 ) -> float:
-    ## @brief Calculates the coefficient of lift for a body.
+def flight_coefficients( force: float=0.0, surface_area: float=0.0, fs_dp: float=0.0, m_arm: float=None ) -> float:
+    ## @brief Calculates the coefficient for a body depding  on what force is
+    # inputed into the function.
+    # Coefficient of Lift = force / (fs_dp * surface_area)
+    # Coefficient of Drag, Normal Force, and Axial force computed in same way
+    # as CL.
+    # Moment Coefficient = force / (fs_dp * surface_area * m_arm )
     # Page 24
+    # 
     #
-    # @param lift_force: total lift upon the body (N)
+    # @param force: total force upon the body (N)
     # @param surface_area: total surface area lift is acting upon (m²)
+    # @param fs_dp: free stream dynamic pressure (Pa)
+    # @param m_arm: moment arm (m)
     # @return coefficient of lift (no units)
-    return lift_force / (fs_dynamic_pressure * surface_area )
+
+    if m_arm is None:
+        return force / (fs_dp * surface_area )
+    else:
+        return force / (fs_dp * surface_area * m_arm )
--- tests/test_BasicLift.py
+++ tests/test_BasicLift.py
@@ -6,6 +6,6 @@ from aerodynamics.BasicLift import *
 def test_freestream_dynamic_pressure():
     assert 0.5 == freestream_dynamic_pressure( 1, 1 )
 
-def test_coefficient_lift():
+def test_flight_coefficients():
     fs_pressure = freestream_dynamic_pressure( 1, 1 )
-    assert 2 == coefficient_lift( 1, 1, fs_pressure )
+    assert 2 == flight_coefficients( 1, 1, fs_pressure ) # lift
