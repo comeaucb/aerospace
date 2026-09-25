8bcfddda230909d1fa04e04a7961c81ec59c0a6a
Cameron B Comeau <cameron.b.comeau@hotmail.com>
2026-09-25 10:05:35 -0600

    Doxygen compliant comments.

--- src/aerodynamics/BasicLift.py
+++ src/aerodynamics/BasicLift.py
@@ -7,23 +7,20 @@ import numpy as np
 from scipy.optimize import root_scalar
 
 def freestream_dynamic_pressure( fs_density: float=0.0, fs_velocity: float=0.0 ) -> float:
+    ## @brief: Implements the free stream dynamics pressure; the pressure far 
+    # ahead of the airfoil (far enough that the airfoil being there has no effect).
     # Page 24
-    # Implements the free stream dynamics pressure; the pressure far ahead of 
-    # the airfoil (far enough that the airfoil being there has no effect)
-
-    # fs_density
-    # density of free stream fluid (kg/m³)
-    # fs_velocity
-    # velocity of fluid (m/s)
-    # return is free stream pressure (Pa)
+    # 
+    # @param fs_density: density of free stream fluid (kg/m³)
+    # @param fs_velocity: velocity of fluid (m/s)
+    # @return Free stream pressure (Pa)
     return 0.5 * fs_density * pow(fs_velocity, 2)
 
 def coefficient_lift( lift_force: float=0.0, surface_area: float=0.0, fs_dynamic_pressure: float=0.0 ) -> float:
+    ## @brief Calculates the coefficient of lift for a body.
     # Page 24
-    # Calculates the Coefficient of Lift
-    # lift_force
-    # total lift upon the body (N)
-    # surface_area
-    # Total surface area lift is acting upon (m²)
-    # returns coefficient of lift (no units)
+    #
+    # @param lift_force: total lift upon the body (N)
+    # @param surface_area: total surface area lift is acting upon (m²)
+    # @return coefficient of lift (no units)
     return lift_force / (fs_dynamic_pressure * surface_area )
